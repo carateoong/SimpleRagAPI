ee224f5ba06f187444931df35876a2d2901fc155
carateoong <35482144+carateoong@users.noreply.github.com>
2025-01-02 17:46:32 -0800

    Edit api so the post method should be working

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from sentence_transformers import SentenceTransformer
 
 import dataReader
@@ -6,7 +8,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
 
-
+app = FastAPI()
 
 model_id = "sentence-transformers/all-MiniLM-L6-v2"
 model = SentenceTransformer("all-MiniLM-L6-v2")
@@ -20,25 +22,31 @@ vector_dimension = embeddings.shape[1]
 # Create a FAISS Index with correct dimensions
 index = faiss.IndexFlatL2(vector_dimension)
 
-app = FastAPI()
-
 texts = []
+class Sentence(BaseModel):
+    id: int
+    text: str
+
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "perasdasdsio"}
 
 
 @app.post("/ingest")
-def ingest(text):
-    # Generate embedding for the input text
-    embedding = model.encode([text])
+def ingest(document: List[Sentence]):
 
-    # Add embedding to FAISS index
-    index.add(embedding)
-    texts.append(text)
+    for sentence in document:
+        # Generate embedding for the input text
+        embedding = model.encode([sentence.text])
 
-    return {"text": text}
+        # Add embedding to FAISS index
+        index.add(embedding)
+        texts.append(sentence.text)
+
+
+    print(texts)
+    return {"ingested text": document}
 
 
 @app.get("/query")
@@ -55,12 +63,3 @@ def query(text: str):
 
     return {"closest text": closest_text, "distance": distances[0][0]}
 
-
-for s in sentences:
-    ingest(s)
-
-
-
-ingest("this is an example sentence")
-print(texts)
-query("this is an example sentence")
